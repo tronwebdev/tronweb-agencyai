b5a6d757fa01cdf4cb211f2ad9896078dfa2b1da
hannah <hannahadora97@gmail.com>
2023-08-14 20:53:00 +0100

    update on pages

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <index-hero />
+    <frame-50 />
+    <heyjv />
+    <WelcomeAgencyAI />
+    <YourCustomer />
+    <FrameBoundGroupOne />
+    <!-- <FrameContest /> -->
+    <the-funnel />
+    <huge-affiliates />
+    <need-anything-sect />
+    <copy-right />
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style></style>
---
+++ pages/others.vue
@@ -0,0 +1,30 @@
+<template>
+  <main>
+    <OtherpagesHero2 />
+    <main class="md:mt-[-80px]">
+      <div class="flex md:flex-row flex-col-reverse md:items-end items-center md:w-[80%]">
+        <div class="lg:mr-[34px] md:mr-[16px] mr-[0] mb-[40px]">
+            <img class="" src="/icons/consult_phot_n_bg.svg" alt="photo" />
+        </div>
+        <div class="bg-[#F9FAFF] md:rounded-[40px] rounded-[20px] lg:px-[40px] md:px-[24px] px-[16px] lg:py-[50px] md:py-[24px] py-[20px] md:w-[60%] w-[90%] mx-auto">
+          <h3 class="lg:text-[60px] md:text-[40px] text-[30px] text-[#232323] font-[700] md:leading-[72px]">
+            Get Free Consultation
+          </h3>
+          <p class="md:text-[18px] text-[16px] text-[#232323]">Fill out your details Below to get a free Consultation.</p>
+          <form class="lg:mt-[50px] md:mt-[40px] mt-[24px]">
+            <input class="px-[24px] py-[20px] w-full border rounded-[100px] border-[#DCDCDC] bg-transparent lg:mb-[30px] md:mb-[24px] mb-[20px]" type="text" name="name" placeholder="Full Name">
+            <input class="px-[24px] py-[20px] w-full border rounded-[100px] border-[#DCDCDC] bg-transparent md:mb-[40px] mb-[25px]" type="email" name="email" placeholder="Email Address">
+            <button class="w-full bg-[#AB3ADD] md:py-[21px] py-[16px] md:px-[32px] px-[20px] md:text-[20px] text-[16px] text-[#fff] rounded-[100px]">
+                Get Started
+            </button>
+          </form>
+        </div>
+      </div>
+    </main>
+    <OtherpagesReviews/>
+  </main>
+</template>
+
+<script setup lang="ts"></script>
+
+<style></style>
